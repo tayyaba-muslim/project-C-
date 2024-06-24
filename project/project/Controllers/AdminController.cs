69ada6eb11d71d7d61b018ca2d9f917a479e6fa1
Tayyaba Muslim <130833312+tayyaba-muslim@users.noreply.github.com>
2024-06-24 12:50:37 +0500

    Updated

---
+++ project/project/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Product prd)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("data is all good");
+            }
+
+                return View();
+        }
+    }
+    
+}
---
+++ project/project/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.Models
+{
+    public class Product
+    {
+        [Key]
+        public int pid { get; set;  }
+        [Required]
+        public string pname { get; set;  }
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")]
+        public string email { get; set;  }
+        [Required]
+        public int price { get; set;  }
+        [Required]
+        public int qty { get; set;  }
+    }
+}
